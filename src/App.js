035eec5ffb9050a44ae8221a67d54840deb31f3c
lilianastrajescu <liliana.strajescu96@gmail.com>
2023-06-01 20:59:07 +0100

    Dark mode button created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+
+
 export default function App() {
+  document.getElementById("theme-toggle").addEventListener("click", (e) => {
+    const checked = e.target.checked;
+    document.body.setAttribute("theme", checked ? "dark" : "light");
+  });
   return (
     <div className="App">
-      <form className="form-inline my-2 my-lg-0">
-        <input
-          className="form-control mr-sm-2"
-          type="search"
-          placeholder="Enter a City"
-          aria-label="Search"
-        />
-        <button className="btn my-2 my-sm-0" type="submit">
-          Search
-        </button>
-      </form>
+     
+      <div class="container">
+    <input type="checkbox" id="theme-toggle"/>
+    <label for="theme-toggle"></label>
+    
+ 
 
-      <footer>
-        <a href="https://github.com/lilianastrajescu/weather-app-react">
-          Open source Code by Liliana Strajescu
-        </a>
-      </footer>
+        <form className="form-inline my-2 my-lg-0">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Enter a City"
+            aria-label="Search"
+          />
+          <button className="btn my-2 my-sm-0" type="search">
+            Search
+          </button>
+        </form>
+
+        <footer>
+          <a href="https://github.com/lilianastrajescu/weather-app-react">
+            Open source Code by Liliana Strajescu
+          </a>
+        </footer>
+      </div>
     </div>
+    
   );
+  
 }
