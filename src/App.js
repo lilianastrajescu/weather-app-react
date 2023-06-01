f16ffa4ba3add7f1b82f72817848824d879d71b6
lilianastrajescu <liliana.strajescu96@gmail.com>
2023-06-01 19:45:59 +0100

    Re created app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <form className="form-inline my-2 my-lg-0">
+        <input
+          className="form-control mr-sm-2"
+          type="search"
+          placeholder="Enter a City"
+          aria-label="Search"
+        />
+        <button className="btn my-2 my-sm-0" type="submit">
+          Search
+        </button>
+      </form>
+
+      <footer>
+        <a href="https://github.com/lilianastrajescu/weather-app-react">
+          Open source Code by Liliana Strajescu
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
-
-export default App;
