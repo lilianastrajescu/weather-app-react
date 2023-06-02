69c58747ef3f3770ab1c153dca0a7df4e4e8d476
lilianastrajescu <liliana.strajescu96@gmail.com>
2023-06-02 17:34:28 +0100

    Dark mode and app skeleton
    - Dark mode modified from Js to react
    - App implemented

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
-
-
+import ThemeToggle from "./ThemeToggle";
 
 export default function App() {
-  document.getElementById("theme-toggle").addEventListener("click", (e) => {
-    const checked = e.target.checked;
-    document.body.setAttribute("theme", checked ? "dark" : "light");
-  });
   return (
-    <div className="App">
-     
-      <div class="container">
-    <input type="checkbox" id="theme-toggle"/>
-    <label for="theme-toggle"></label>
-    
- 
-
+    <div className="App container">
+      <ThemeToggle />
+      <div>
         <form className="form-inline my-2 my-lg-0">
           <input
             className="form-control mr-sm-2"
@@ -28,15 +18,32 @@ export default function App() {
             Search
           </button>
         </form>
-
-        <footer>
-          <a href="https://github.com/lilianastrajescu/weather-app-react">
-            Open source Code by Liliana Strajescu
-          </a>
-        </footer>
       </div>
+
+      <div className="first-section container">
+        <div className="row">
+          <div className="col-4 col-md-8">
+            <div class="row">
+              <div class="col-sm-12">Wind</div>
+              <div class="col-sm-12">Humidity</div>
+              <div class="col-sm-12">How are you</div>
+            </div>
+          </div>
+          <div className="col-6 col-md-4">10 C</div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-2">1 Sec</div>
+          <div className="col-sm-2">2 Sec</div>
+          <div className="col-sm-2">3 sec</div>
+        </div>
+      </div>
+      <footer>
+        <a href="https://github.com/lilianastrajescu/weather-app-react">
+          Open source Code by Liliana Strajescu
+        </a>
+      </footer>
     </div>
-    
   );
-  
 }
---
+++ src/ThemeToggle.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const ThemeToggle = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleToggle = () => {
+    setIsChecked(!isChecked);
+    document.body.setAttribute("theme", isChecked ? "light" : "dark");
+  };
+
+  return (
+    <div className="dark-mode-section">
+      <input
+        type="checkbox"
+        id="theme-toggle"
+        checked={isChecked}
+        onChange={handleToggle}
+      />
+      <label htmlFor="theme-toggle"></label>
+    </div>
+  );
+};
+
+export default ThemeToggle;
